03db2fc787fbeb76be92021e450061f2f840ae46
Pingfan Liu <piliu@redhat.com>
2025-07-01 20:02:39 +0800

    kexec_file: Factor out routine to find a symbol in ELF
    The routine to search a symbol in ELF can be shared, so split it out.
    
    Signed-off-by: Pingfan Liu <piliu@redhat.com>
    Cc: Baoquan He <bhe@redhat.com>
    Cc: Dave Young <dyoung@redhat.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Philipp Rudo <prudo@redhat.com>
    To: kexec@lists.infradead.org

--- kernel/kexec_internal.h
+++ kernel/kexec_internal.h
@@ -41,6 +41,7 @@ extern int pe_get_section(const char *file_buf, unsigned long buf_sz,
 		const char *sect_name, char **sect_start,
 		unsigned long *sect_sz);
 extern int decompose_kexec_image(struct kimage *image, int extended_fd);
+extern const Elf_Sym *elf_find_symbol(const Elf_Ehdr *ehdr, const char *name);
 #else /* CONFIG_KEXEC_FILE */
 static inline void kimage_file_post_load_cleanup(struct kimage *image) { }
 #endif /* CONFIG_KEXEC_FILE */
